f58c0eef945d17ff6bfe93465d223e730290a337
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-04 20:09:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,10 +2,7 @@ numbers = [1,2,3]
 new_numbers = []
   def square_array(numbers)
   numbers.each do |number|
-  new_number = number**2
-  new_number.each do |new_number|
-  new_numbers << new_number
+  number**2 << new_numbers
   return new_numbers
    end
-end
 end
