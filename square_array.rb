7820f7e0b7aadf66e65fd168c71d6b545cee77a7
Benjamin Morgan-Cleveland <benmorgancleveland@gmail.com>
2020-09-04 20:04:59 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 new_numbers = []
   def square_array(numbers)
   numbers.each do |number|
-  new_numbers =  number**2, number**2, number**2 
+  new_numbers =  "#{number**2, number**2, number**2}" 
   return new_numbers
    end
 end
